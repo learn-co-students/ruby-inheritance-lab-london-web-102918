9027a70313324b3356f97d0ef7633340b43f1aa4
rosen777 <rosen.toshev@flatironschool.com>
2018-11-05 09:03:06 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(student)
+    @knowledge << student
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
