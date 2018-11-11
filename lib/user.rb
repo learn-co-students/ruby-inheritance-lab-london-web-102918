4efce98c31248802e452913ae554897a3b81fae3
joaquimbarreto <joaquim@barreto.co>
2018-11-11 22:16:46 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,12 @@
+class User
+
+    attr_accessor :first_name, :last_name, :knowledge
+    
+    def initialize#(first_name, last_name, knowledge)
+        @first_name = first_name
+        @last_name = last_name
+        @knowledge = []
+    end
+
+
+end
